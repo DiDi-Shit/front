51f5adb5e345fa5e60f89e84c53b94967263fdc6
jzy991126 <jzy991126@outlook.com>
2020-04-14 13:03:29 +0800

    update readme

--- App.js
+++ App.js
@@ -11,47 +11,14 @@ import {
 
 import { Button, ThemeProvider } from 'react-native-elements';
 
-import LKText from './components/LKText'
-import LKImage from './components/LKImage';
-import { MapView, Marker } from 'react-native-amap3d'
-import JMap from './components/JMap'
-
-import AppContainer from './navigator/basenav'
-
-// const App: () => React$Node = () => {
-//   let str = 'ok';
-//   return (
-//     <>
-//       <StatusBar barStyle='dark-content'></StatusBar>
-//       <SafeAreaView style={styles.mainViewStyle}>
-//         <JMap>
-
-//         </JMap>
-
-//         {/* <JMap style={StyleSheet.absoluteFill}></JMap> */}
-//       </SafeAreaView>
-//     </>
-//   )
-// }
-
 const App: () => React$Node = () => {
   return (
     <ThemeProvider>
-      < AppContainer>
-      </ AppContainer>
+      <Text>
+        ok
+      </Text>
     </ThemeProvider>
   );
 };
-
-const styles = StyleSheet.create({
-  mainViewStyle: {
-    flex: 1, backgroundColor: 'red',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: 500,
-    width: 500
-  }
-})
 
 export default App;
